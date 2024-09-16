532b12cfafe6d839be3b72f61a63276f494e6fee
Nesli23 <nesli.kinali@hotmail.com>
2024-09-16 12:04:43 +0200

    Ändrad

--- Java_playground/Book2.java
+++ Java_playground/Book2.java
@@ -2,28 +2,29 @@ package Java_playground;
 
 public class Book2 {
 
-    String title;
-    String author;
-    int year;
+    private String title;
+    private String author;
+    private int year;
 
-    public Book2 (){
-    title = " Sämsta boken";
-    author = " Författare Författaresson";
-    year =  1996;
+    public Book2(){
+    title = " Boksson";
+    author = "Sven Svensson";
+    year = 1996;
 
     }
-    
+
     public void printDetails () {
-       System.out.println("Title" + title);
-       System.out.println("Author" + author);
-       System.out.println("Year" + year);
+        System.out.println("Title" + title);
+        System.out.println("Author" + author);
+        System.out.println("Year" + year);
     }
 
     public static void main(String[] args) {
-        Book2 book = new Book2();
-        book.printDetails();
+        Book book2 = new Book();
+        book2.printDetails();
     }
 }
+
 
 
 
