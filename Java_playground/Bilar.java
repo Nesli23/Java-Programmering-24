988c535df9176f9f46998a238e2bc6787c04bc27
Nesli23 <nesli.kinali@hotmail.com>
2024-09-16 13:58:55 +0200

    Övning 3 Bilar

---
+++ Java_playground/Bilar.java
@@ -0,0 +1,16 @@
+package Java_playground;
+
+public class Bilar {
+    String make;
+    String model;
+    int year;
+    String color;
+
+    public Bilar(String make, String model, int year, String color)
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+
+    }
+}
