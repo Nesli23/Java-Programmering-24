df9d33c6b9958ea2d1f0727192b79acc8e2fc056
Nesli23 <nesli.kinali@hotmail.com>
2024-09-16 13:10:49 +0200

    Övning 3 Students

---
+++ Java_playground/Students.java
@@ -0,0 +1,25 @@
+package Java_playground;
+
+public class Students {
+
+    public String name;
+    public int age;
+    public double grade;
+
+    public Students(){
+        name = " Kalle";
+        age = 25;
+        grade = 1.5;
+    }
+    
+    public void printDetails(){
+        System.out.println(" Name" + name);
+        System.out.println("Age" + age);
+        System.out.println("Grade" + grade);
+    }
+
+    public static void main(String[] args) {
+        Students students = new Students();
+        students.printDetails();
+    }
+}
