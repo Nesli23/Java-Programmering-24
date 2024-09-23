8ab290003a682109051ec611508e2ac84014b75b
Nesli23 <nesli.kinali@hotmail.com>
2024-09-23 12:54:21 +0200

    Övning 2

---
+++ Java_playground/Result.java
@@ -0,0 +1,9 @@
+package Java_playground;
+
+public class Result {
+    public static void main(String[] args) {
+        int result = 0;
+        for (int x = 1, y = 10; x < 5; x++, y--)
+            result = result + x * y;
+    }
+}
