bc6a8cfa9698f86c9fe19d83ee24f0e227d9c3e5
Nesli23 <nesli.kinali@hotmail.com>
2024-09-23 11:59:33 +0200

    Övning 4

---
+++ Java_playground/MainMethodKeywords.java
@@ -0,0 +1,8 @@
+package Java_playground;
+
+public class MainMethodKeywords {
+        public static void main(String[] args) {
+            System.out.print("main is working fine!");
+        }
+    }
+
