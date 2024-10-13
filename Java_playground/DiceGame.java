2d8b5f8faacc8fa9a4ade8fb6f7a2d803ba2f616
Nesli23 <nesli.kinali@hotmail.com>
2024-10-13 19:01:23 +0200

    Ändring

--- Java_playground/DiceGame.java
+++ Java_playground/DiceGame.java
@@ -1,5 +1,7 @@
 package Java_playground;
 
+import java.util.Scanner;
+
 public class DiceGame {
     public static void main(String[] args) {
         
@@ -8,30 +10,35 @@ public class DiceGame {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Namn:");
-        String namne = scanner.nextLine();
+        String name = scanner.nextLine();
 
-        System.out.println("Skriv in antal omgångar du vill spela");
-        int rounds = scanner.nextLine();
+        System.out.println("Skriv in antal omgångar du vill spela: ");
+        int rounds = scanner.nextInt();
 
         System.out.println("Hur många sidor ska det vara på tärningen?");
         int numberOfSides = scanner.nextInt();
-        scanner.nextLine
+        scanner.nextLine();
 
-        player= new Player(name, numberOfSides);
+        player = new Player(name, numberOfSides);
 
-        for(int round = 1; round <= rounds, round++) {
+        for(int round = 1; round <= rounds; round++) {
             System.out.println("Omgång" + round);
             System.out.println(player.getName() + ", gissa tärningsvärdet:");
-            int guess = scanner.nextInt():
+            int guess = scanner.nextInt();
 
             player.rollDice();
         int dieValue = player.getDieValue();
         System.out.println(player.getName() + " rullade " + dieValue);
 
-        player.rollDice();
-        int dieValue = player.getDieValue();
-        System.out.println(player.getName() + " rullade " + dieValue);
-
-        System.out.println("Spelet är slut. " + player.getName() + " har " + player.getScore() + " poäng.");
+        if (guess == dieValue) {
+            System.out.println(player.getName() + " får en poäng!");
+            player.increaseScore();
+        } else {
+            System.out.println(player.getName() + " får inte en poäng!");
+        }
     }
+
+    System.out.println("Spelet är slut. " + player.getName() + " har " + player.getScore() + " poäng.");
+    }
+
 }
