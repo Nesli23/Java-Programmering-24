b95c273569863e5d619f449185195249d4dcb8fd
Nesli23 <nesli.kinali@hotmail.com>
2024-10-18 09:07:39 +0200

    Ändringar

---
+++ Tärningsspel/DiceGame.java
@@ -0,0 +1,44 @@
+package Java_playground;
+
+import java.util.Scanner;
+
+public class DiceGame {
+    public static void main(String[] args) {
+        
+        Player player;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Namn:");
+        String name = scanner.nextLine();
+
+        System.out.println("Skriv in antal omgångar du vill spela: ");
+        int rounds = scanner.nextInt();
+
+        System.out.println("Hur många sidor ska det vara på tärningen?");
+        int numberOfSides = scanner.nextInt();
+        scanner.nextLine();
+
+        player = new Player(name, numberOfSides);
+
+        for(int round = 1; round <= rounds; round++) {
+            System.out.println("Omgång" + round);
+            System.out.println(player.getName() + ", gissa tärningsvärdet:");
+            int guess = scanner.nextInt();
+
+            player.rollDice();
+        int dieValue = player.getDieValue();
+        System.out.println(player.getName() + " rullade " + dieValue);
+
+        if (guess == dieValue) {
+            System.out.println(player.getName() + " får en poäng!");
+            player.increaseScore();
+        } else {
+            System.out.println(player.getName() + " får inte en poäng!");
+        }
+    }
+
+    System.out.println("Spelet är slut. " + player.getName() + " har " + player.getScore() + " poäng.");
+    }
+
+}
---
+++ Tärningsspel/Player.java
@@ -0,0 +1,46 @@
+package Java_playground;
+    
+public class Player {
+    private String name;
+    private int score;
+    public Die die;
+ 
+    public Player(String name, int numberOfSides) {
+        this.name = name;
+        score = 0;
+        this.die = new Die(numberOfSides);
+ 
+    }
+   
+    public String getName() {
+      return name;
+    }
+ 
+    public int getScore() {
+      return score;
+    }
+ 
+    public void setScore(int score) {
+      this.score = score;
+    }
+ 
+    public void rollDice() {
+        die.roll();
+ 
+    }
+ 
+    public int getDieValue() {
+      return die.getValue();
+    }
+ 
+    public void increaseScore() {
+      score++;
+    }  
+ 
+    public void addDie(int numberOfSides) {
+       die = new Die(numberOfSides);
+      }
+ 
+ 
+}
+ 
