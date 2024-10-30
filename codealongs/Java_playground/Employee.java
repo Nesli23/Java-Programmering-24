0821a9a5e4fcca53ec855932b69817065aa7b850
Nesli23 <nesli.kinali@hotmail.com>
2024-10-30 12:34:19 +0100

    Skapat övning Employees

---
+++ codealongs/Java_playground/Employee.java
@@ -0,0 +1,20 @@
+package codealongs.Java_playground;
+
+public class Employee {
+    public String name;
+    public int age;
+
+    public Employee(String name, int age){
+        this.name = name;
+        this.age = age;
+
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getage() {
+        
+    }
+}
