e7085f7a6581df8c9618c99b1c1306e9e207528a
Nesli23 <nesli.kinali@hotmail.com>
2024-11-16 14:22:10 +0100

    Ny klass med instruktioner

---
+++ codealongs/Java_playground/InstructionText.java
@@ -0,0 +1,8 @@
+package codealongs.Java_playground;
+
+public class InstructionText {
+    public String getInstructions() {
+        return "Instruktioner till QuizGame spelet, hej och hå";
+    
+    }
+}
