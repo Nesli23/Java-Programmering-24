6c072ca4d4c3f120d36d10ca9ff46e0f6a992d30
Nesli23 <nesli.kinali@hotmail.com>
2024-11-16 13:44:49 +0100

    Skapar nytt fönster

---
+++ codealongs/Java_playground/Instruction.java
@@ -0,0 +1,11 @@
+package codealongs.Java_playground;
+
+import javax.swing.*;
+
+public class Instruction {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Instructions");
+        frame.setSize(350,250);
+        frame.setVisible(true);
+    }
+}
