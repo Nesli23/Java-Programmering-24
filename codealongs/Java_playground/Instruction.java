36c4e105ae8ce3ea8a1e38b50a113106514b6223
Nesli23 <nesli.kinali@hotmail.com>
2024-11-16 13:56:44 +0100

    Korrigerat kod

--- codealongs/Java_playground/Instruction.java
+++ codealongs/Java_playground/Instruction.java
@@ -6,6 +6,8 @@ public class Instruction {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Instructions");
         frame.setSize(350,250);
+        JLabel label = new JLabel("Instruktioner till QuizGame", JLabel.CENTER);
+        frame.add(label);
         frame.setVisible(true);
     }
 }
