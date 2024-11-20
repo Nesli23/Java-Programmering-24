979e5cfd33c6c0c2d0063315220aa5a3b5fb7a7b
Nesli23 <nesli.kinali@hotmail.com>
2024-11-20 08:52:42 +0100

    Lagt till information

--- codealongs/Java_playground/Instruction.java
+++ codealongs/Java_playground/Instruction.java
@@ -4,10 +4,15 @@ import javax.swing.*;
 
 public class Instruction {
     public static void main(String[] args) {
+        InstructionText instructionText = new InstructionText();
         JFrame frame = new JFrame("Instructions");
         frame.setSize(350,250);
-        JLabel label = new JLabel("Instruktioner till QuizGame", JLabel.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JLabel label = new JLabel(instructionText.getInstructions(), JLabel.CENTER);
         frame.add(label);
+        //JButton button = new JButton("Tryck här för instruktioner");
         frame.setVisible(true);
+        //frame.add(button);
+        
     }
 }
