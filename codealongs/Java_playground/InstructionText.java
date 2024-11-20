35256ed1a62defc144d11311b2ad69d96d3deb75
Nesli23 <nesli.kinali@hotmail.com>
2024-11-20 08:52:50 +0100

    Lagt till information

--- codealongs/Java_playground/InstructionText.java
+++ codealongs/Java_playground/InstructionText.java
@@ -1,8 +1,11 @@
 package codealongs.Java_playground;
 
 public class InstructionText {
-    public String getInstructions() {
-        return "Instruktioner till QuizGame spelet, hej och hå";
-    
-    }
+  public String getInstructions(){
+    return "Välkommen till QuizGame!\n" +
+               "För att spela, välj ett ämne och börja svara på frågorna.\n" +
+               "Om du får rätt på alla frågor, kommer du att vinna!" +
+               "Kom ihåg att varje fråga har en tidsgräns.\n" +
+               "Lycka till och ha kul!";
+}
 }
